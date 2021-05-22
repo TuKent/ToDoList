29b192ceae54849674fd754277f67334155e0827
TuNguyen <yuki0035vn@gmail.com>
2021-05-22 11:17:02 +0700

    up the first

---
+++ react-app-todolist/src/App.js
@@ -0,0 +1,10 @@
+// import logo from './logo.svg';
+// import './App.css';
+import SinglePage from "./scenes/main-page/single-page";
+function App() {
+  return (
+    <SinglePage></SinglePage>
+  );
+}
+
+export default App;
